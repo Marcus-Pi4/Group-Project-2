7e03436412f5441015446b5c716f199daf6d6c96
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-08-11 20:50:09 -0700

    Initial boilerplates

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Turn on routes
+app.use(routes);
+
+// Connection to server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
