ba201f4c7b9e00a17ec009de5bf92c2997d2f8e4
eva <83175995+evapopp@users.noreply.github.com>
2021-08-14 12:40:10 -0700

    testing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // NYT Newswire API - fetches the most up to date NYT publications for our default publications to render on page load - can filter 'all', 'nyt' and 'inyt' for the first 'all' and similar to above, there is a section list available to input into the second 'all
+
 function getTopStories(){
     fetch(`https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=QuEHRaQh47l8vSF9ewprDtUY6fj2IJz6`)
     .then(function (response){
@@ -11,8 +12,12 @@ function renderTopStoriesData(data){
     console.log(data)
 }
 
+
+
 // NYT Top Stories API - fetches top stories based on 'section' - where section options can be from the following list: arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, t-magazine, travel, upshot, us, world
 var userQuery = 'arts'
+
+const 
 
 function getNewYorkTimes(section){
     fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=QuEHRaQh47l8vSF9ewprDtUY6fj2IJz6`)
@@ -44,3 +49,5 @@ function handleGuardData(data) {
 }
 
 getGuardian(userSearch)
+
+// test
