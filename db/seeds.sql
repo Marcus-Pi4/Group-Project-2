de4384a91196a8a8ef1062076d38cf0a162db10c
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-08-16 19:43:34 -0700

    Users to input a current email as opposed to creating unique username

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (id, username, password)
+INSERT INTO users (id, email, password)
 VALUES
-    (1, "rachael123", "PaSsWorD180");
+    (1, "rachael123@fakeemail.com", "PaSsWorD180");
