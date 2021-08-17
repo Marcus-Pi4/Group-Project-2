5003191a34d77109600c45a49a489654a7db9bd0
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-08-16 21:28:59 -0700

    Added SequelizeStore

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 const mysql = require('mysql2');
 const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
