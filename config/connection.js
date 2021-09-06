88403fec08fb4512ab09efadf5d4e7567ed38459
Marcus Reynolds <marcusroy93@gmail.com>
2021-09-06 12:13:37 -0700

    fixed jawsdb Maria env variable

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-    host: process.env.JAWSDB,
+    host: process.env.JAWSDB_MARIA_URL,
     dialect: 'mysql',
     }
 );
