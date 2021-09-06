1dcc130c0827a4b7a24cf12617f0cd656bcba9a2
Marcus Reynolds <marcusroy93@gmail.com>
2021-09-06 14:08:56 -0700

    reverted previous change

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-    host: process.env.HOST,
-    dialect: 'mysql',
-    }
-);
+const sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL);
 
 module.exports = sequelize;
