70b6e94bb1ecf64bd21e69927620b1f1065b1ec8
Marcus Reynolds <marcusroy93@gmail.com>
2021-09-07 21:05:18 -0700

    edited connection.js sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL);
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_MARIA_URL
+  ? new Sequelize(process.env.JAWSDB_MARIA_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
