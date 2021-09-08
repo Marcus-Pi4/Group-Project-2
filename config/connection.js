5b527f3589f96f42bf108e18e10566d5d2614a5b
Marcus Reynolds <marcusroy93@gmail.com>
2021-09-07 21:29:58 -0700

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: process.env.HOST,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
